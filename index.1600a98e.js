b23c39d76f43508aceb3e5c2a9c57488a9e50feb
associate2coder <olatsko@gmail.com>
2024-09-28 10:27:05 +0300

    make build

---
+++ index.1600a98e.js
@@ -0,0 +1,2 @@
+function r(r){var e=Array.from(r.children),n=e[e.length-1],o=e[1].cloneNode(!0);n.before(o)}for(var e=Array.from(document.querySelector("table").children),n=0;n<e.length;n++)1===n?Array.from(e[n].children).forEach(function(e){return r(e)}):r(e[n].firstElementChild);
+//# sourceMappingURL=index.1600a98e.js.map
